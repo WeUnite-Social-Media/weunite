54798ce63c8a40ff6ec6990ce0d5d64b94bba705
matheushahah <78930361+Matheus-fast@users.noreply.github.com>
2026-03-28 09:16:37 -0300

    fix(OpportunityDTO): update opportunity DTO to improve clean code

--- apps/api/src/main/java/com/weunite/api/admin/stats/dto/OpportunityCategoryWithSkillsDTO.java
+++ apps/api/src/main/java/com/weunite/api/admin/stats/dto/OpportunityCategoryWithSkillsDTO.java
@@ -1,19 +1,5 @@
 package com.weunite.api.admin.stats.dto;
-
 import java.util.List;
 
 public record OpportunityCategoryWithSkillsDTO(
-    String category, Long count, List<OpportunitySkillDTO> topSkills) {
-
-  public String getCategory() {
-    return category;
-  }
-
-  public Long getCount() {
-    return count;
-  }
-
-  public List<OpportunitySkillDTO> getTopSkills() {
-    return topSkills;
-  }
-}
+    String category, Long count, List<OpportunitySkillDTO> topSkills) {}
