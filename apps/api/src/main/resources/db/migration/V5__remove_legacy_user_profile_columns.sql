a99ee92c4ec5d4b7eec45b5777122ae55d5db6a3
matheushahah <78930361+Matheus-fast@users.noreply.github.com>
2026-05-30 09:48:11 -0300

    fix(api): make profile column migration CI-compatible

--- apps/api/src/main/resources/db/migration/V5__remove_legacy_user_profile_columns.sql
+++
@@ -1,7 +0,0 @@
-alter table tb_user drop column cpf;
-alter table tb_user drop column height;
-alter table tb_user drop column weight;
-alter table tb_user drop column foot_domain;
-alter table tb_user drop column position;
-alter table tb_user drop column birth_date;
-alter table tb_user drop column cnpj;
